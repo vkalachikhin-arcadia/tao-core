893ce558e2e4074e94da217ffbfba0396eb6affa
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-10-24 15:21:24 +0200

    media selector structure

---
+++ views/js/ui/mediaSelector/selector.js
@@ -0,0 +1,85 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ */
+
+/**
+ * A switch component, toggles between on and off
+ *
+ * @example
+ * switchFactory(container, config)
+ *     .on('change', function(value){
+ *              console.log('The light is ' + value);
+ *     });
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'i18n',
+    'ui/component',
+    'ui/resource/selector',
+    'tpl!ui/mediaSelector/tpl/selector',
+    'ui/previewer',
+    'css!ui/mediaSelector/css/selector.css'
+], function($, _, __, component, resourceSelectorFactory, switchTpl, previewer){
+    'use strict';
+
+    var states = {
+    };
+
+    var defaultConfig = {
+    };
+
+    /**
+     * The factory that creates a switch component
+     *
+     * @param {jQueryElement} $container - where to append the component
+     * @returns {switchComponent} the component
+     */
+    return function switchFactory($container, config){
+
+        /**
+         * The component API
+         */
+        var api = {
+
+        };
+
+        var switchComponent = component(api, defaultConfig)
+            .setTemplate(switchTpl)
+            .on('init', function(){
+
+
+                this.render($container);
+            })
+            .on('render', function(){
+                var self = this;
+
+                var $component = this.getElement();
+
+                resourceSelectorFactory($component.find('.resource-selector-container'));
+
+$component.find('.media-preview').previewer({ url : 'test.mp4', type : 'video/mp4' });
+            });
+
+        _.defer(function(){
+            switchComponent.init(config);
+        });
+        return switchComponent;
+    };
+});
---
+++ views/js/ui/mediaSelector/tpl/selector.tpl
@@ -0,0 +1,25 @@
+<div class="media-selector">
+    <section class="media-browser">
+        <div class="resource-selector-container"></div>
+        <ul class="actions plain action-bar tree-action-bar">
+            <li class="action"><a href="#" class="li-inner"><span class="glyph icon-move-item"></span>{{__ 'Select'}}</a></li>
+            <li class="action"><a href="#" class="li-inner"><span class="glyph icon-bin"></span>{{__ 'Delete'}}</a></li>
+            <li class="action"><a href="#" class="li-inner"><span class="glyph icon-download"></span>{{__ 'Download'}}</a></li>
+            <li class="action"><a href="#" class="li-inner"><span class="glyph icon-upload"></span>{{__ 'Upload'}}</a></li>
+        </ul>
+    </section>
+
+    <section class="media-view">
+        <div class="media-preview">
+            Preview
+
+        </div>
+        <div class="media-properties">
+            Properties
+        </div>
+    </section>
+    <section class="media-upload hidden">
+        Upload
+    </section>
+
+</div>
